5248867c15a2d8556329a46d22acbaca16faad7c
Belkacz <belka.lukasz.job@gmail.com>
2024-11-14 15:46:11 +0100

    #2 changes in server,hs script and htmls

--- js/script.js
+++ js/script.js
@@ -5,22 +5,19 @@ function redirectTo(url) {
     window.open(url, "_blank")
 }
 
-// funkcja która wycina nam pcozatek adresu do ostatniego znaku '/' a następnie dodaje nowy endpoint
+// funkcja która wycina nam pcozatek adresu do ostatniego znaku '/' a następnie dodaje nowy endpoint po #
 function redirectToSubPage(destination, event) {
-    event.preventDefault()
+    event.preventDefault() // blokujemy przełdowanie by nie było efektu klimay przenosi nas na strone #<nazwa_sub_domeny> a następnie stona się odświerza i znów
+                            // trafiamy na stonę domową bez #
     const path = window.location.pathname;
-    if (destination == '' || destination == null || destination == undefined) {
+    if (destination == '' || destination == null || destination == undefined) { // przy pusty endponcie wróć na stornę główną
         window.location.href = path
     } else {
         window.location.href = path + '#' + destination;
     }
-
-    const mobileMenu = document.getElementById('mobile-menu');
-    if (!mobileMenu.classList.contains('hidden')) {
-        mobileMenu.classList.add('absolute')
-    }
 }
 
+//funkcja przesuwajaca na góre
 function scrollToTop() {
     window.scrollTo({
         top: 0,
@@ -28,6 +25,7 @@ function scrollToTop() {
     });
 }
 
+// funkcja pokazujaca i chowająca listę pod mobile menu
 function showMobileMenu() {
     const mobileMenu = document.getElementById('mobile-menu');
     if (mobileMenu.classList.contains('hidden')) {
@@ -37,6 +35,8 @@ function showMobileMenu() {
     }
 }
 
+// funkcja do chowania wysiniętego menu jak powiększymy ekran. Problem był taki że jak się wywineło mobile menu porzeszyło ekran i znów go zmniejszyło to
+// menu było ciągle wywinięte i to było dziwne. 
 function toggleMobileMenuVisibility() {
     const mobileMenu = document.getElementById('mobile-menu');
     if (window.innerWidth >= 1024) {
@@ -46,6 +46,7 @@ function toggleMobileMenuVisibility() {
     }
 }
 
+// asynchroniczbna funkcja do wysyłania informacji z formualrz kontaktowego
 async function sendContact(data, form) {
     try {
         const response = await fetch("http://localhost:3000/save-contact", {
@@ -57,18 +58,21 @@ async function sendContact(data, form) {
         });
 
         if (response.ok) {
-            alert("Formularz został wysłany pomyślnie.");
+            alert("Twój formualrz został wysłany :)");
             form.reset();
         } else {
-            alert("Wystąpił błąd podczas wysyłania formularza.");
+            alert("Błąd w trakcie wysyłania formualrza.");
         }
     } catch (error) {
         console.error("Błąd:", error);
-        alert("Wystąpił problem z połączeniem z serwerem.");
+        alert("Błąd w trakcie łączenia z serwerem.");
     }
 }
 
+// funkcja inicjująca validatory formularza i blokująca przycisk wyślij. Musi to być tu wyciągniete byśmy nie próbowali złpać inputów gdy ich nie ma
+// na innych stronach. Daltego też właczamy to gdy jesteśmy pod adresem zakończonym #contact
 function initiateContactForm() {
+    const form = document.getElementById("contact-form");
 
     const validationMsg = document.getElementById("validation-msg");
     const name = document.getElementById("input_name");
@@ -83,6 +87,10 @@ function initiateContactForm() {
     const errorEmailMsgDot = document.createElement("p");
     const errorLargeInput = document.createElement("p");
 
+    // walidatorów e mail, delkaracje walidatorów powinny być wyciągniete z ciała funkcji initaiteContactForm, zaś elementy jak errorNameMsg, errorEmailMsg, name itd.
+    // mogłyby być przekazywane jako argument funkcji jednak biorąc pod uwagę że każdy z tych walidatorów włącza i uruchamiana sprawdzenie przycisku checkSubmitButton();
+    // musielibyś też propgaować im te elementy lub twożyć/ łapać QerrySelectorem ten przycisk na nowo, w wyniku czego może nam powstać większe spaghetti niż tego
+    // oczekujemy
     function emailValidator() {
         const trimmedEmail = eMail.value.trim();
         if (!trimmedEmail.includes("@")) {
@@ -113,10 +121,11 @@ function initiateContactForm() {
                 validationMsg.removeChild(errorEmailMsgDot);
             }
         }
-
+        // sprawdzanie blokady przycisku wyślij
         checkSubmitButton();
     }
 
+    // walidator czy pole jest puste, abstrakcyjny trochę by był dla każadego pola
     function validateEmpty(field, fieldError, filedName) {
         if (field.value.trim() === "") {
             fieldError.innerText = `Pole ${filedName} nie może być puste.`;
@@ -130,6 +139,7 @@ function initiateContactForm() {
         checkSubmitButton();
     }
 
+    // no funkcja blokująca lub odblowywująca przycisk
     function checkSubmitButton() {
         const hasErrors = validationMsg.children.length > 0;
         const isNameEmpty = name.value.trim() === "";
@@ -156,25 +166,24 @@ function initiateContactForm() {
         validateEmpty(largeInput, errorLargeInput, "wiadomości");
     });
 
-
-    const form = document.getElementById("contact-form");
-    console.log(form)
+    // event na przycisk submit naszego formularza
     form.addEventListener("submit", (event) => {
-        event.preventDefault();
+        event.preventDefault(); // wyłaczenie przeładowania
 
-        if (validationMsg.children.length === 0) {
+        if (validationMsg.children.length === 0) { //sprawdzenie czy nie ma errodów i spakowanie danych do obiektu data
             const data = {
                 name: name.value.trim(),
                 email: eMail.value.trim(),
                 message: largeInput.value.trim()
             };
-            sendContact(data, form);
+            sendContact(data, form); // wysłanie danych
         }
     });
 }
 
+// funkcja wstrzykujaca załadowane dane
 async function setLoadedData(elementName, div) {
-
+    // blok catch try do łapania błędów
     try {
         const response = await fetch(`http://localhost:3000/${elementName}`);
 
@@ -182,20 +191,23 @@ async function setLoadedData(elementName, div) {
             throw new Error(`Error: ${response.status}`);
         }
         const htmlContent = await response.text();
-        div.innerHTML = htmlContent;
+        div.innerHTML = htmlContent; // wstrzykiwanie danych do elementu div
+
+        // sprawdzenie czy mamy podstonę #contact i zainicjowanie walidatorów formualrza
         if (elementName === "contact") {
             const form = document.getElementById("contact-form");
             if (form) {
                 initiateContactForm();
             }
         }
-    } catch (error) {
+    } catch (error) { // wrzucenie error komunikatu w dany blok jeśli się nie załaduje
         div.innerHTML = `<h3 class="text-xl">Dane elemntu ${elementName} nie załadowane.</h3>`
         div.style.cssText = errorMsgCss;
         console.error('Wystąpił błąd:', error);
     }
 }
 
+// taki wrapper do ładowania danych i wrzucjący palceholder (widoczny przy wolniejszym internecie)
 function tryToLoadData(elementName, div) {
     div.innerHTML = `<div class="p-5 px0 title-container">
         <h1 class="text-4xl text-accent font-bold uppercase title-shadow ">Ładowanie danych ...</h1>
@@ -203,6 +215,7 @@ function tryToLoadData(elementName, div) {
     setLoadedData(elementName, div);
 }
 
+// funkcja sterujaca jakie elementy wczytywać.
 function getDataFromLocalhost(endpoint) {
 
     const navbar = document.getElementById("nav-bar");
@@ -227,6 +240,7 @@ function getDataFromLocalhost(endpoint) {
     }
 }
 
+// event odpalajacy dane na start po załadowaniu strony przy odświeżeniu i pokazywanie lub chowanie mobile menu
 document.addEventListener("DOMContentLoaded", (event) => {
     const address = window.location.href;
     const endpoint = address.slice(address.indexOf('#') + 1);
@@ -234,10 +248,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
     window.addEventListener("resize", toggleMobileMenuVisibility);
 });
 
+// event na zmaine wartości za # na kończu adresu by wychwycić przejscie an subdomene i pobrać dane
 window.addEventListener('hashchange', () => {
     const address = window.location.href;
     const endpoint = address.slice(address.indexOf('#') + 1);
-    console.log('Endpoint after hashchange:', endpoint);
     getDataFromLocalhost(endpoint);
 });
 
--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const cors = require('cors');
+const port = 3000
+
 
 app.use(cors());
 
 app.use(express.json());
 
+// odebranie z post fomualrza kontaktowego.
 app.post('/save-contact', (req, res) => {
   const { name, email, message } = req.body;
   console.log("Server odebrał i jest gotowy do wysłania danych:");
@@ -15,62 +18,69 @@ app.post('/save-contact', (req, res) => {
   res.json({ message: "Formularz został zapisany!" });
 });
 
+// zwrócienie na get navbarku strony
 app.get('/nav-bar', (req, res) => {
   const filePath = path.join(__dirname, 'views', 'nav-bar.html');
-
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
-      res.status(500).send('Błąd serwera: nie udało się wczytać stopki');
+      res.status(500).send('Błąd serwera: nie udało się wczytać paska nawigacji.');
       return;
     }
     res.send(data);
   });
 });
 
+// zwrócienie na get stopki strony
 app.get('/footer', (req, res) => {
   const filePath = path.join(__dirname, 'views', 'footer.html');
-
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
-      res.status(500).send('Błąd serwera: nie udało się wczytać stopki');
+      res.status(500).send('Błąd serwera: nie udało się wczytać stopki.');
       return;
     }
     res.send(data);
   });
 });
 
+// zwrócienie na get głównej strony
 app.get('/main', (req, res) => {
   const filePath = path.join(__dirname, 'views', 'main.html');
 
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
-      res.status(500).send('Błąd serwera: nie udało się wczytać stopki');
+      res.status(500).send('Błąd serwera: nie udało się wczytać strony głównej.');
       return;
     }
     res.send(data);
   });
 });
 
+// zwrócienie na get galerii
 app.get('/gallery', (req, res) => {
   const filePath = path.join(__dirname, 'views', 'gallery.html');
-
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
-      res.status(500).send('Błąd serwera: nie udało się wczytać stopki');
+      res.status(500).send('Błąd serwera: nie udało się wczytać galerii.');
       return;
     }
     res.send(data);
   });
 });
 
+// zwrócienie na get formularza kontakt
 app.get('/contact', (req, res) => {
   const filePath = path.join(__dirname, 'views', 'contact.html');
-
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
-      res.status(500).send('Błąd serwera: nie udało się wczytać stopki');
+      res.status(500).send('Błąd serwera: nie udało się wczytać fomualrza kontaktowego');
       return;
     }
     res.send(data);
   });
 });
+
+// uruchomienie serwera na danym porcie
+app.listen(port, () => {
+  console.log(`Serwer działa na localhost:${port}`)
+})
+
